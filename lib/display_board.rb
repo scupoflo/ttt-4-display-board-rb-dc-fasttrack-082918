9fab613138822b1b5be79713bdf99856f808d366
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-09-30 18:08:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," ","X"," "," "," "," "]
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board (board)
